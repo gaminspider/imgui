f0fd7a901b451df0bc6c2d08ddc08263399104fc
Simon <simon.bartholomeusz@gmail.com>
2025-03-06 21:38:15 +1100

    tweaked premake5.lua and generated project

--- premake5.lua
+++ premake5.lua
@@ -8,14 +8,14 @@ project "imgui"
 
 	files
 	{
-		"imgui.cpp",
-		"imgui_demo.cpp",
-		"imgui_draw.cpp",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp",
         "imconfig.h",
+        "imgui.cpp",
         "imgui.h",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
         "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
         "imstb_rectpack.h",
         "imstb_textedit.h",
         "imstb_truetype.h",
